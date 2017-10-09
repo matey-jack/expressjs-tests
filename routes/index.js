08d273dc8017060e21a04ba6ce371d90ae845e17
Robert Will <r.will@funkedigital.de>
2017-10-09 15:54:10 +0200

    add error different ways of handling, one dysfunctional and one correct

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,49 @@ const CoreService = require('../lib/CoreService');
 
 const router = express.Router();
 
-/* test results: in both cases errors are caught by express framework, however:
+/* test results for errors
+
+in both cases errors are caught by express framework and don't crash the server.
+
+however:
  - async error (above) never returns anything to browser! 
    (connection seems to stay open, very bad!)
  - sync error (below) returns a 500 page with error stack trace (good!)
 */
-router.get('/', function(req, res, next) {
+router.get('/async', function(req, res, next) {
   CoreService.heavyLifting().then( () => {
     res.render('index', { title: 'Express' });
-  })
+  });
 });
 
-router.get('/test', function(req, res, next) {
+router.get('/sync', function(req, res, next) {
   const test = misspelledVariable;
   res.render('index', { title: 'Express' });
 });
 
-router.get('/', function(req, res, next) {
+/* test results for catching
+
+rethrowing an error has absolutely no effect and is wasted code!
+
+exceptions and expecially rejected promises from services have to be caugth in 
+the controller, because this is the right place to forward them to the error handler
+by calling next() with the error object!
+*/
+
+router.get('/badcatch', function(req, res, next) {
   CoreService.heavyLifting().then( () => {
     res.render('index', { title: 'Express' });
+  }).catch((err) => {
+    throw err;
   })
 });
 
+router.get('/goodcatch', function(req, res, next) {
+  CoreService.heavyLifting().then( () => {
+    res.render('index', { title: 'Express' });
+  }).catch((err) => {
+    next(err);
+  })
+});
 
 module.exports = router;
